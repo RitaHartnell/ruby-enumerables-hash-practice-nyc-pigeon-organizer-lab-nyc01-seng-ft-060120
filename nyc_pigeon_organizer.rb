596e61db55a6a6270a8e970e8cff3e4db6f8b4f3
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-12 01:02:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each { |key, value|
+    value.each {|key2, intern|
+      intern.collect {|val|
+        new_hash[val][key] << key2.to_s
+      }
+    }
+  }
 end
